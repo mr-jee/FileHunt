3307b04940679ced70f6fa25d503fcfa5355cdd7
mr-jee <navidaria333@gmail.com>
2023-07-15 10:15:17 +0330

    sorting the csv by type

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 starting_directory = "D:\\"
 
+
 def scan_files(directory):
     file_list = []
 
@@ -18,39 +19,17 @@ def scan_files(directory):
                 file_list.append((dir_name, dir_path, "directory"))
     return file_list
 
+
 def save_to_csv(file_list, csv_file):
     df = pd.DataFrame(file_list, columns=['Name', 'Address', 'Type'])
-    df.to_csv(csv_file, index= False)
+    df.sort_values("Type", axis=0, ascending=True, inplace=True, na_position='first')
+    df.to_csv(csv_file, index=False)
 
 
 files = scan_files(starting_directory)
 
 csv_file = 'list_of_files.csv'
 save_to_csv(files, csv_file)
-print(files)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # ------------------ UI&UX-------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
@@ -71,10 +50,8 @@ searchbox_entry = Entry(width=70)
 searchbox_entry.grid(row=1, column=0, columnspan=2)
 searchbox_entry.focus()
 
-
 search_btn = Button(text='Search')
 search_btn.grid(row=1, column=2, padx=5, pady=5)
-
 
 result_list = Listbox(width=90, height=10)
 result_list.grid(row=2, column=0, columnspan=3, padx=5)
